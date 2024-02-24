643266afb35396427a600a0a54d7b4bc7916f4fe
yura-prots <yuraprots@gmail.com>
2024-02-24 20:04:08 +0200

    made memoized selector

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,14 +1,16 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectContacts = state => state.contacts;
 export const selectFilter = state => state.filter;
 
-export const selectVisibleContacts = state => {
-  const contacts = selectContacts(state);
-  const filter = selectFilter(state);
+export const selectVisibleContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) => {
+    return contacts.filter(contact => {
+      const contactName = contact.name.toLowerCase().trim();
+      const filterName = filter.toLowerCase().trim();
 
-  return contacts.filter(contact => {
-    const contactName = contact.name.toLowerCase().trim();
-    const filterName = filter.toLowerCase().trim();
-
-    return contactName.includes(filterName);
-  });
-};
+      return contactName.includes(filterName);
+    });
+  }
+);
