52e0ee56ac6f922d842dcc20f0eedbf96abea227
yura-prots <yuraprots@gmail.com>
2024-02-25 21:45:13 +0200

    add handlers

--- src/redux/contacts/slice.js
+++ src/redux/contacts/slice.js
@@ -5,6 +5,15 @@ import {
   deleteContact,
 } from '../../redux/operations';
 
+const handlePending = state => {
+  state.isLoading = true;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+
 export const contactsSlice = createSlice({
   name: 'contacts',
   initialState: {
@@ -13,17 +22,31 @@ export const contactsSlice = createSlice({
     error: null,
   },
   extraReducers: builder => {
-    builder.addCase(fetchContacts.fulfilled, (state, action) => {
-      state.items = action.payload;
-    });
-    builder.addCase(addContact.fulfilled, (state, action) => {
-      state.items.push(action.payload);
-    });
-    builder.addCase(deleteContact.fulfilled, (state, action) => {
-      const index = state.items.findIndex(
-        contact => contact.id === action.payload.id
-      );
-      state.items.splice(index, 1);
-    });
+    builder
+      .addCase(fetchContacts.pending, handlePending)
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.items = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(fetchContacts.rejected, handleRejected);
+
+    builder
+      .addCase(addContact.pending, handlePending)
+      .addCase(addContact.fulfilled, (state, action) => {
+        state.items.push(action.payload);
+        state.isLoading = false;
+      })
+      .addCase(addContact.rejected, handleRejected);
+
+    builder
+      .addCase(deleteContact.pending, handlePending)
+      .addCase(deleteContact.fulfilled, (state, action) => {
+        const index = state.items.findIndex(
+          contact => contact.id === action.payload.id
+        );
+        state.items.splice(index, 1);
+        state.isLoading = false;
+      })
+      .addCase(deleteContact.rejected, handleRejected);
   },
 });
