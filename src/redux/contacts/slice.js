e5fd2ef0bd107c99e10cf9a6e61ffcfe9425e0d4
yura-prots <yuraprots@gmail.com>
2024-02-25 13:27:39 +0200

    changed deleteContacts

--- src/redux/contacts/slice.js
+++ src/redux/contacts/slice.js
@@ -12,7 +12,9 @@ export const contactsSlice = createSlice({
       state.items.push(action.payload);
     },
     deleteContact(state, action) {
-      return state.filter(contact => contact.id !== action.payload);
+      state.items = state.items.filter(
+        contact => contact.id !== action.payload
+      );
     },
   },
 });
